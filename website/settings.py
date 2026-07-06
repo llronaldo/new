1541c6df8da125e856e126a91b956418113248b5
l小小菜鸟l <14033782+l-little-rookie-l@user.noreply.gitee.com>
2026-07-07 02:19:33 +0800

    改回SQLite，保留CSRF修复

--- website/settings.py
+++ website/settings.py
@@ -100,15 +100,8 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'KjuKEEkVVtUpJzrKWUCdJddEAIqPgWcG',
-        'HOST': 'mysql.railway.internal',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
