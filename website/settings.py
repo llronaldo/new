f77a5095cb4f2e1ce63adc0f033905a931bc99ab
l小小菜鸟l <14033782+l-little-rookie-l@user.noreply.gitee.com>
2026-07-07 02:10:24 +0800

    切换到MySQL数据库

--- website/settings.py
+++ website/settings.py
@@ -100,8 +100,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'KjuKEEkVVtUpJzrKWUCdJddEAIqPgWcG',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
