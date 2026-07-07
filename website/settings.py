845ca83aa72c232d2325d0c1fb621b3df978b071
l小小菜鸟l <14033782+l-little-rookie-l@user.noreply.gitee.com>
2026-07-07 16:28:16 +0800

    恢复SQLite，移除Supabase配置

--- website/settings.py
+++ website/settings.py
@@ -100,15 +100,8 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'LLliuleiqwer123',
-        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-        'PORT': '6543',
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
