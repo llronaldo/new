1dcbdfce2e6c94dbe74a7263a8de215c828cae8a
l小小菜鸟l <14033782+l-little-rookie-l@user.noreply.gitee.com>
2026-07-09 03:30:21 +0800

    chore: force Railway redeploy

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ python manage.py collectstatic --noinput
 echo "=== Running migrations ==="
 python manage.py migrate --noinput
 
-echo "=== Build complete ==="
+echo "=== Build complete ==="# Cache bust Thu Jul  9 03:30:21     2026
